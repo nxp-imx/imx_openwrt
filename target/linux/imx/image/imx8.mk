a862b2d3d29187ef3ff7eec84b6a4b9e74df68fb
Yuantian Tang <andy.tang@nxp.com>
2022-07-01 15:42:56 +0800

    imx: add imx8mplus platform support
    Imx8mplus_evk device is built to meet the needs of Smart Home,
    Building, City and Industry 4.0, 5G gateway and router  applications.
    
    Hardware specifications:
    CPU: 4x or 2x Cortex-A53 up to 1.8 GHz
    DRAM: 32-bit DDR4 & LPDDR4 up to 4.0GT/s
    Board: imx8mp_evk
    SoC: imx8mplus
    On-board WiFi/Bluetooth chip: NXP 88W8997
    Ethernet: 2x Gigabit Ethernet with AVB, IEEE 1588, EEE and 1x w/ TSN
    
    Signed-off-by: Yuantian Tang <andy.tang@nxp.com>

---
+++ target/linux/imx/image/imx8.mk
@@ -0,0 +1,98 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright 2022 NXP
+
+# 16MB bootloader + 40MB kernel
+IMX_SD_KERNELPART_SIZE = 40
+IMX_SD_KERNELPART_OFFSET = 16
+IMX_SD_ROOTFSPART_OFFSET = 64
+IMX_SD_IMAGE_SIZE = $(shell echo $$((($(IMX_SD_ROOTFSPART_OFFSET) + \
+	$(CONFIG_TARGET_ROOTFS_PARTSIZE)))))
+
+define Build/imx-clean
+	# Clean the target
+	rm -f $@
+endef
+
+define Build/imx-compile-dtb
+	# Compile dts file to dtb
+	$(call Image/BuildDTB,$(DTS_DIR)/$(DEVICE_DTS).dts,$(DTS_DIR)/$(DEVICE_DTS).dtb)
+endef
+
+define Build/imx-create-flash
+	# Combile firmware + bl31 + uboot to flash.bin
+	cd $(STAGING_DIR_IMAGE)/$(MKIMG_DIR) && $(MAKE) SOC=$(PLAT) $(DEVICE_TYPE)
+endef
+
+define Build/imx-append
+	# append binary
+	dd if=$(STAGING_DIR_IMAGE)/$(1) >> $@
+endef
+
+define Build/imx-append-boot
+	# Append the uboot, firmware etc.
+	dd if=$(STAGING_DIR_IMAGE)/$(MKIMG_DIR)/$(SOC_TYPE)/flash.bin >> $@
+endef
+
+define Build/imx-append-dtb
+	# Append the dtb file
+	dd if=$(DTS_DIR)/$(1).dtb >> $@
+endef
+
+define Build/imx-append-kernel
+	# append the kernel
+	mkdir -p $@.tmp && \
+	cp $(IMAGE_KERNEL) $@.tmp && \
+	cp $(DTS_DIR)/$(DEVICE_DTS).dtb $@.tmp && \
+	make_ext4fs -J -L kernel -l "$(IMX_SD_KERNELPART_SIZE)M" "$@.kernel.part" "$@.tmp" && \
+	dd if=$@.kernel.part >> $@ && \
+	rm -rf $@.tmp && \
+	rm -f $@.kernel.part
+endef
+
+define Build/imx-append-sdhead
+	# Create the sd file table
+	./gen_sdcard_head_img.sh $(STAGING_DIR_IMAGE)/$(1)-sdcard-head.img \
+		$(IMX_SD_KERNELPART_OFFSET) $(IMX_SD_KERNELPART_SIZE) \
+		$(IMX_SD_ROOTFSPART_OFFSET) $(CONFIG_TARGET_ROOTFS_PARTSIZE)
+	dd if=$(STAGING_DIR_IMAGE)/$(1)-sdcard-head.img >> $@
+endef
+
+define Device/Default
+  PROFILES := Default
+  FILESYSTEMS := squashfs
+  KERNEL_INITRAMFS = kernel-bin
+  KERNEL_LOADADDR := 0x80080000
+  KERNEL_ENTRY_POINT := 0x80080000
+  IMAGE_SIZE := 64m
+  KERNEL = kernel-bin
+  IMAGES := sdcard.img sysupgrade.bin
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+
+define Device/imx8mplus
+  DEVICE_VENDOR := NXP
+  DEVICE_MODEL := IMX8MPLUS
+  DEVICE_VARIANT := SD Card Boot
+  PLAT := iMX8MP
+  SOC_TYPE := iMX8M
+  DEVICE_TYPE := flash_evk
+  ENV_NAME:=imx8mp-sdboot
+  MKIMG_DIR:= `find $(STAGING_DIR_IMAGE) -name imx-mkimage* | xargs basename`
+  DEVICE_PACKAGES += \
+	atf-imx8mp \
+	firmware-imx \
+	imx-mkimage \
+	u-boot-imx8mp
+  DEVICE_DTS := freescale/imx8mp-evk
+  IMAGE/sdcard.img := \
+	imx-compile-dtb | \
+	imx-create-flash | \
+	imx-clean | \
+	imx-append-sdhead $(1) | pad-to 32K | \
+	imx-append-boot | pad-to 4M | \
+	imx-append $$(ENV_NAME)-uboot-env.bin | pad-to $(IMX_SD_KERNELPART_OFFSET)M | \
+	imx-append-kernel | pad-to $(IMX_SD_ROOTFSPART_OFFSET)M | \
+	append-rootfs | pad-to $(IMX_SD_IMAGE_SIZE)M
+endef
+TARGET_DEVICES += imx8mplus
---
+++ target/linux/imx/imx8/target.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright 2022 NXP
+
+ARCH:=aarch64
+BOARDNAME:=NXP i.MX8 boards
+KERNELNAME:=Image
+
+define Target/Description
+	Build firmware images for NXP imx8 boards.
+endef
