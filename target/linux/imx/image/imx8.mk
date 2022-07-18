db5884a1b0e975b1c991bca3009a94ac0e37a90d
Yuantian Tang <andy.tang@nxp.com>
2022-07-18 14:38:00 +0800

    imx: add imx8mnano platform support
    Signed-off-by: Yuantian Tang <andy.tang@nxp.com>

--- target/linux/imx/image/imx8.mk
+++ target/linux/imx/image/imx8.mk
@@ -123,3 +123,30 @@ define Device/imx8mmini
 	append-rootfs | pad-to $(IMX_SD_IMAGE_SIZE)M
 endef
 TARGET_DEVICES += imx8mmini
+
+define Device/imx8mnano
+  DEVICE_VENDOR := NXP
+  DEVICE_MODEL := IMX8MNANO
+  DEVICE_VARIANT := SD Card Boot
+  PLAT := iMX8MN
+  SOC_TYPE := iMX8M
+  DEVICE_TYPE := flash_evk
+  ENV_NAME:=imx8mn-sdboot
+  MKIMG_DIR:= `find $(STAGING_DIR_IMAGE) -name imx-mkimage* | xargs basename`
+  DEVICE_PACKAGES += \
+	atf-imx8mn \
+	firmware-imx \
+	imx-mkimage \
+	u-boot-imx8mn
+  DEVICE_DTS := freescale/imx8mn-evk
+  IMAGE/sdcard.img := \
+	imx-compile-dtb $$(DEVICE_DTS) | \
+	imx-create-flash $$(PLAT) $$(DEVICE_TYPE) | \
+	imx-clean | \
+	imx-append-sdhead $(1) | pad-to 32K | \
+	imx-append-boot $$(SOC_TYPE) | pad-to 4M | \
+	imx-append $$(ENV_NAME)-uboot-env.bin | pad-to $(IMX_SD_KERNELPART_OFFSET)M | \
+	imx-append-kernel $$(DEVICE_DTS) | pad-to $(IMX_SD_ROOTFSPART_OFFSET)M | \
+	append-rootfs | pad-to $(IMX_SD_IMAGE_SIZE)M
+endef
+TARGET_DEVICES += imx8mnano
