4470e34cf2860014fb2bf06426a9dc3592e91ec4
Yuantian Tang <andy.tang@nxp.com>
2024-12-27 09:29:07 +0800

    imx: add imx93evk support
    Signed-off-by: Andy Tang <andy.tang@nxp.com>

--- target/linux/imx/image/cortexa55.mk
+++ target/linux/imx/image/cortexa55.mk
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0-only
 #
-# Copyright 2023 NXP
+# Copyright 2023-2024 NXP
 
 define Device/Default
   PROFILES := Default
@@ -16,10 +16,10 @@ endef
 
 define Device/imx93evk
   DEVICE_VENDOR := NXP
-  DEVICE_MODEL := IMX93
+  DEVICE_MODEL := IMX93evk
   DEVICE_VARIANT := SD Card Boot
   BOARD_NAME := iMX9
-  SOC_TYPE := iMX9
+  SOC_TYPE := iMX93
   BOOT_TYPE := flash_singleboot
   ENV_NAME:=imx93-sdboot
   DEVICE_PACKAGES += \
@@ -34,7 +34,7 @@ define Device/imx93evk
 	imx-create-flash $$(BOARD_NAME) $$(BOOT_TYPE) | \
 	imx-clean | \
 	imx-append-sdhead $(1) | pad-to 32K | \
-	imx-append-boot $$(SOC_TYPE) | pad-to 4M | \
+	imx-append-boot $$(SOC_TYPE) | pad-to 7M | \
 	imx-append $$(ENV_NAME)-uboot-env.bin | pad-to $(IMX_SD_KERNELPART_OFFSET)M | \
 	imx-append-kernel $$(DEVICE_DTS) | pad-to $(IMX_SD_ROOTFSPART_OFFSET)M | \
 	append-rootfs | pad-to $(IMX_SD_IMAGE_SIZE)M
